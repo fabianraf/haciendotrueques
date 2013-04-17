89cf39e974ff90bb6157fd925dba48bc06261968
Fabian Aguirre <faguirre@nettheory.com>
2013-04-17 00:12:52 -0500

    .gitignore funciona

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haciendotrueques::Application.config.secret_token = '5e60c8bbf70b3d329f2b18632e9ed281cb0b89829d39f84c5497fe399f72df514c6a7224e86959d96e91bd4d6e9da1fcb84506eba62a923fa6a1bc2fd0174448'
